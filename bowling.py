c427bb8c64eae51817d7274705a57e9455730fac
negzi <negarin.fazeli@gmail.com>
2014-04-16 13:28:20 +0200

    Update bowling.py

--- bowling.py
+++ bowling.py
@@ -1,33 +1,42 @@
-class BowlingGame:
+class Game:
 
-    def calculate_points(self, pint_hits_in_game):
-        total_score = 0
-        for i, pin_hits_in_frame in enumerate(pint_hits_in_game):
-            total_score += sum(pin_hits_in_frame)
-            if self.is_spare(pin_hits_in_frame) and i < (len(pint_hits_in_game) - 1):
-                total_score += pint_hits_in_game[i+1][0]
-            if self.is_strike(pin_hits_in_frame) and i < (len(pint_hits_in_game) - 2):
-                total_score += pint_hits_in_game[i+1][0]
-                if self.is_strike(pint_hits_in_game[i+1]):
-                    total_score += pint_hits_in_game[i+2][0]
-                else:
-                    total_score += pint_hits_in_game[i+1][1]
-        return total_score
+    def calculate_points(self, game):
+        score = 0
+        for i, frame in enumerate(game):
+            score += sum(frame)
+            if i == 9:
+                score += self.handle_last_frame(frame, game)
+                break
+            else:
+                if self.is_spare(frame) and i < (len(game) - 1):
+                    score += game[i+1][0]
+                if self.is_strike(frame) and i < (len(game) - 2):
+                    score += self.get_2_balls(i, game)
+        return score
 
-    def is_spare(self, pin_hits_in_frame):
-        is_spare = False
-        if pin_hits_in_frame[0] != 10 and sum(pin_hits_in_frame) == 10:
-            is_spare = True
-        return is_spare
+    def handle_last_frame(self, frame, game):
+        # return after spare and after strike
+        score = 0
+        if self.is_spare(frame):
+            score += game[10][0]
+        if self.is_strike(frame):
+            score += self.get_2_balls(9, game)
+        return score
 
-    def is_strike(self, pin_hits_in_frame):
-        is_strike = False
-        if pin_hits_in_frame[0] == 10:
-            is_strike = True
-        return is_strike
+    def is_spare(self, frame):
+        return frame[0] != 10 and sum(frame) == 10
+
+    def is_strike(self, frame):
+        return frame[0] == 10
+
+    def get_2_balls(self, frame_id, game):
+        sum_p = sum(game[frame_id + 1])
+        if self.is_strike(game[frame_id + 1]):
+            sum_p += game[frame_id + 2][0]
+        return sum_p
 
 
-    # problem: 11th frame we add pin hits and the bonus
+
 
 
 
