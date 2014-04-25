9b4c911f8be6d47a7c4ea2f0a3e998b8c85aec4b
negzi <negarin.fazeli@gmail.com>
2014-04-25 09:47:34 +0200

    Refacored

--- bowling.py
+++ bowling.py
@@ -1,17 +1,16 @@
 class Game:
 
-    def calculate_points(self, game):
+    def calc_score(self, game):
         score = 0
+        LAST_FRAME = 9
         for i, frame in enumerate(game):
             score += sum(frame)
-            if i == 9:
-                score += self.handle_last_frame(frame, game)
-                break
-            else:
-                if self.is_spare(frame) and i < (len(game) - 1):
+            if i == LAST_FRAME:
+                return score + self.handle_last_frame(frame, game)
+            elif self.is_spare(frame):
                     score += game[i+1][0]
-                if self.is_strike(frame) and i < (len(game) - 2):
-                    score += self.get_2_balls(i, game)
+            elif self.is_strike(frame):
+                    score += self.get_bonus(i, game)
         return score
 
     def handle_last_frame(self, frame, game):
@@ -20,7 +19,7 @@ class Game:
         if self.is_spare(frame):
             score += game[10][0]
         if self.is_strike(frame):
-            score += self.get_2_balls(9, game)
+            score += self.get_bonus(9, game)
         return score
 
     def is_spare(self, frame):
@@ -29,22 +28,9 @@ class Game:
     def is_strike(self, frame):
         return frame[0] == 10
 
-    def get_2_balls(self, frame_id, game):
-        sum_p = sum(game[frame_id + 1])
+    def get_bonus(self, frame_id, game):
+        bonus = sum(game[frame_id + 1])
         if self.is_strike(game[frame_id + 1]):
-            sum_p += game[frame_id + 2][0]
-        return sum_p
+            bonus += game[frame_id + 2][0]
+        return bonus
 
-
-
-
-
-
-
-
-
-
-
-
-# issue: i+2 is     total_score += total_score[i+2][0]
-# IndexError: list index out of range
--- bowlingunittest.py
+++ bowlingunittest.py
@@ -5,53 +5,63 @@ class BowlingTest(unittest.TestCase):
 
     def test_first(self):
         pinHits = [[2,6]]+[[0,0]]*9
-        self.assertEqual(8, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(8, bowling.Game().calc_score(pinHits))
 
     def test_2(self):
         pinHits = [[0, 0]]
-        self.assertEqual(0, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(0, bowling.Game().calc_score(pinHits))
 
     def test_3(self):
         pinHits = [[2, 0], [5, 0]]
-        self.assertEqual(7, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(7, bowling.Game().calc_score(pinHits))
 
     def test_spares(self):
         pinHits = [[5, 5], [5, 0]]
-        self.assertEqual(20, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(20, bowling.Game().calc_score(pinHits))
 
     def test_spares2(self):
         pinHits = [[5, 5], [5, 0], [3,7],[1, 0]]
-        self.assertEqual(32, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(32, bowling.Game().calc_score(pinHits))
 
     def test_spares3(self):
         pinHits = [[10, 0], [5, 1]] +[[0,0]]*8
-        self.assertEqual(22, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(22, bowling.Game().calc_score(pinHits))
 
     def test_strikes1(self):
         pinHits = [[10, 0], [10, 0], [10, 0]] +[[0,0]]*7
-        self.assertEqual(60, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(60, bowling.Game().calc_score(pinHits))
 
     def test_spares4(self):
         pinHits = [[0, 0]]*9 +[[9,1],[2,0]]
-        self.assertEqual(12, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(12, bowling.Game().calc_score(pinHits))
 
     def test_strike2(self):
         pinHits = [[0, 0]]*9+ [[10, 0], [10, 0], [10, 0]]
-        self.assertEqual(30, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(30, bowling.Game().calc_score(pinHits))
 
     def test_score_of_next_2_balls(self):
         pinHits = [[0,0],[10,0], [10, 0]]
         current_frame = 0
-        self.assertEqual(20, bowling.Game().get_2_balls(current_frame, pinHits))
+        self.assertEqual(20, bowling.Game().get_bonus(current_frame, pinHits))
 
     def test_score_of_next_2_balls2(self):
         pinHits = [[0,0],[5,3]]
         current_frame = 0
-        self.assertEqual(8, bowling.Game().get_2_balls(current_frame, pinHits))
+        self.assertEqual(8, bowling.Game().get_bonus(current_frame, pinHits))
 
     def test_strike4(self):
         pinHits = [[0, 0]]*9+ [[10, 0], [2, 1]]
-        self.assertEqual(13, bowling.Game().calculate_points(pinHits))
+        self.assertEqual(13, bowling.Game().calc_score(pinHits))
+
+    def test_gutter_game(self):
+        pinHits = [[0, 0]]*10
+        self.assertEqual(0, bowling.Game().calc_score(pinHits))
+
+    def test_perfect_game(self):
+        pinHits = [[10, 0]]*12
+        self.assertEqual(300, bowling.Game().calc_score(pinHits))
+
+
 
 
 
